89e5f4902a0da027f7f1b5023bb21c7a0ef294da
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2012-11-15 02:22:01 -0200

    inicia classe de busca de tweet

---
+++ src/twitter/Finder.java
@@ -0,0 +1,11 @@
+package twitter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phedro
+ * Date: 11/15/12
+ * Time: 2:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Finder {
+}
