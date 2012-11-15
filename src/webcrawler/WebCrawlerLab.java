14cd4287caf359df51e6493aaf9c74c4e6c4b21b
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2012-11-15 17:24:59 -0200

    inicia construtor do crawler

--- src/webcrawler/WebCrawlerLab.java
+++ src/webcrawler/WebCrawlerLab.java
@@ -1,5 +1,8 @@
 package webcrawler;
 
+import websphinx.Crawler;
+import websphinx.DownloadParameters;
+
 /**
  * Created with IntelliJ IDEA.
  * User: phedro
@@ -7,5 +10,16 @@ package webcrawler;
  * Time: 3:48 PM
  * To change this template use File | Settings | File Templates.
  */
-public class WebCrawlerLab {
+public class WebCrawlerLab extends Crawler
+{
+    public WebCrawlerLab()
+    {
+        super();
+        DownloadParameters params = new DownloadParameters();
+        params.changeObeyRobotExclusion(true);
+        params.changeUserAgent("MyCrawler Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.4) WebSPHINX 0.5 contact me_at_mycompany_dot_com");
+        setDownloadParameters(params);
+        setDomain(Crawler.SUBTREE);
+        setLinkType(Crawler.HYPERLINKS);
+    }
 }
