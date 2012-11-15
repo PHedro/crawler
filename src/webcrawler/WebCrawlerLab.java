9c35f208c380709c2f099f48d1d5c9a4eeecb4a7
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2012-11-15 17:37:56 -0200

    refactoring e override shouldVisit e visit, método whileInPage

--- src/webcrawler/WebCrawlerLab.java
+++ src/webcrawler/WebCrawlerLab.java
@@ -2,6 +2,11 @@ package webcrawler;
 
 import websphinx.Crawler;
 import websphinx.DownloadParameters;
+import websphinx.Link;
+import websphinx.Page;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,14 +17,46 @@ import websphinx.DownloadParameters;
  */
 public class WebCrawlerLab extends Crawler
 {
-    public WebCrawlerLab()
+    private static String pattern = "";
+
+    public WebCrawlerLab(String url, int maxDepth, String pattern)
     {
         super();
-        DownloadParameters params = new DownloadParameters();
-        params.changeObeyRobotExclusion(true);
-        params.changeUserAgent("MyCrawler Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.4) WebSPHINX 0.5 contact me_at_mycompany_dot_com");
-        setDownloadParameters(params);
-        setDomain(Crawler.SUBTREE);
-        setLinkType(Crawler.HYPERLINKS);
+        try {
+            DownloadParameters params = new DownloadParameters();
+            params.changeObeyRobotExclusion(true);
+            params.changeUserAgent("MyCrawler Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.8.1.4) WebSPHINX 0.5 contact me_at_mycompany_dot_com");
+            setDownloadParameters(params);
+            setDomain(Crawler.SUBTREE);
+            setLinkType(Crawler.HYPERLINKS);
+            setMaxDepth(maxDepth);
+            setRoot(new Link(new URL(url)));
+            this.pattern = pattern;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void visit(Page page) {
+        whileInPage(page);
+        try
+        {
+            Thread.sleep(500L);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean shouldVisit(Link link) {
+        URL url = link.getURL();
+        return (url.toString().contains(pattern));
+    }
+
+    protected void whileInPage(Page page) {
+        URL url = page.getURL();
+        System.out.println(url.toString());
     }
 }
