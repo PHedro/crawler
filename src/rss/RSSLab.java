e27faeccb7cf9b706ea3c471520a49fc4dd9dcdf
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2012-11-15 09:31:02 -0200

    inicia RSSLab

---
+++ src/rss/RSSLab.java
@@ -0,0 +1,11 @@
+package rss;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phedro
+ * Date: 11/15/12
+ * Time: 9:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RSSLab {
+}
