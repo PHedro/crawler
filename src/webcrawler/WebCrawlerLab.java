29ed9fb1afe1da3fc6cd0baee41fed4de217895e
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2012-11-15 15:49:04 -0200

    inicia webcrawler

---
+++ src/webcrawler/WebCrawlerLab.java
@@ -0,0 +1,11 @@
+package webcrawler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phedro
+ * Date: 11/15/12
+ * Time: 3:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WebCrawlerLab {
+}
