4bbc15c5f18e6467b4a2a6fe4cab0753352e23df
Pedro Henrique Reis <pedrohenrique.reis@gmail.com>
2012-11-15 05:23:52 -0200

    inicia GmailLab

---
+++ src/gmail/GmailLab.java
@@ -0,0 +1,11 @@
+package gmail;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: phedro
+ * Date: 11/15/12
+ * Time: 5:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GmailLab {
+}
